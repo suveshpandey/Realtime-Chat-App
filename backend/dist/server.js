a38d0b24c69b0cbcaac8b3578c561084483f61ee
Suvesh Pandey <jpsuvesh29@gmail.com>
2025-03-02 23:47:14 +0530

    updated backend url in frontend

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -7,12 +7,14 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
 const auth_route_1 = __importDefault(require("./routes/auth.route"));
 const message_route_1 = __importDefault(require("./routes/message.route"));
 const db_1 = require("./lib/db");
 const socket_1 = require("./lib/socket");
 dotenv_1.default.config();
 const PORT = process.env.PORT;
+const __dirname = path_1.default.resolve();
 // app.use(express.json());
 socket_1.app.use(express_1.default.json({ limit: "10mb" })); // Increase JSON payload size limit
 socket_1.app.use(express_1.default.urlencoded({ limit: "10mb", extended: true }));
@@ -23,6 +25,12 @@ socket_1.app.use((0, cors_1.default)({
 }));
 socket_1.app.use('/api/auth', auth_route_1.default);
 socket_1.app.use('/api/messages', message_route_1.default);
+if (process.env.NODE_ENV === "production") {
+    socket_1.app.use(express_1.default.static(path_1.default.join(__dirname, ".../frontend/dist")));
+    socket_1.app.get("*", (req, res) => {
+        res.sendFile(path_1.default.join(__dirname, ".../frontend", "dist", "index.html"));
+    });
+}
 socket_1.server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
     (0, db_1.connectDB)();
--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
-    // baseURL: "https://talkify-by-sp.onrender.com/api",
+    // baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+    baseURL: "https://talkify-by-sp.onrender.com/api",
     withCredentials: true
 })
